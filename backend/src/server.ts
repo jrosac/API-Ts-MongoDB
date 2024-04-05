2fc90a3896e74694458ae5bf954846f578b7086a
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-04-05 12:50:43 -0300

    Updated the package.json

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+
+const app = fastify({logger: true});
+
+const start = async () =>{
+    try{
+        await app.listen({port: 3333})
+    }catch(err){
+        process.exit(1)
+    }
+}
